11991b8e94da1c2a3c3058d13a7459d554ee8d1e
pratik03071995 <108683914+pratik03071995@users.noreply.github.com>
2024-02-03 14:05:36 -0500

    two sum update

--- Arrays_and_Hashing/TwoSum.py
+++ Arrays_and_Hashing/TwoSum.py
@@ -1,1 +1,21 @@
 # Your Python code goes here
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(0, len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i,j
+
+
+
+check = Solution()
+nums = [11,2,15,7]
+target = 17
+print(check.twoSum(nums,target))
