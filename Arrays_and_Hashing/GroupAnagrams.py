30d6748b12d661c452f7ac537e399acd0c00c85f
pratik03071995 <108683914+pratik03071995@users.noreply.github.com>
2024-02-03 17:35:55 -0500

    Update Group Anagram

--- Arrays_and_Hashing/GroupAnagrams.py
+++ Arrays_and_Hashing/GroupAnagrams.py
@@ -1,1 +1,34 @@
-# Your Python code goes here
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        main_anagram_list = []
+        for i in range(0,len(strs)):
+            my_anagram_list = []
+            word_list1 = list(strs[i])
+            for j in range(i+1,len(strs)):
+                word_list2 = list(strs[j])
+                if len(word_list1) == len(word_list2):
+                    for k in range(0,len(word_list1)):
+                        if word_list1[k] in word_list2:
+                            word_list2.remove(word_list1[k])
+
+                if len(word_list2) == 0 :
+                    if strs[i] not in my_anagram_list:
+                        my_anagram_list.append(strs[i])
+                    my_anagram_list.append(strs[j])
+
+            main_anagram_list.append(my_anagram_list)
+
+        return main_anagram_list
+
+                   
+
+check = Solution()
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(check.groupAnagrams(strs))
+
+
